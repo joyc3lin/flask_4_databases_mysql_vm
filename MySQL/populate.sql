43cc3cfd470d9a67dee962ffadd382264ed5bac7
Joyce Lin <123987150+joyc3lin@users.noreply.github.com>
2023-10-11 23:49:36 -0400

    Create populate.sql

---
+++ MySQL/populate.sql
@@ -0,0 +1,15 @@
+INSERT INTO patients (first_name, last_name, date_of_birth) VALUES
+('Sarah', 'Jones', '1992-03-24'),
+('Vanessa', 'Potter', '2001-08-31'),
+('Herman', 'Shyler', '1996-10-11');
+
+INSERT INTO demographics (preferred_pronouns, languages_spoken, address, phone_number, email) VALUES
+('she/her', 'English', '1234 Gerry Street', '345643', 'jonessar@yahoo.com'),
+('she/her', 'English, French', '4231 Lime Ave', '876945', 'VanOtter@gmail.com'),
+('he/his', 'English', '6543 Hungry Rd', '986035', 'ShyHerler@gmail.com');
+
+INSERT INTO patient_preferences (favorite_food, favorite_shows, hobbies) VALUES
+('pizza', 'Game of Thrones', 'hiking'),
+('ramen', 'Harry Potter', 'football'),
+('rice', 'One Piece', 'sleeping');
+ 
